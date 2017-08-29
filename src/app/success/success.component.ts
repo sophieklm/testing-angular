f3e5acc0fe79df555fa11d98dddabeb024c76e0c
sophieklm <sklm@live.co.uk>
2017-08-29 12:15:20 +0100

    updates alert styles

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-success-alert',
-  template: '<p class="text-success"> Success! <p>'
+  template: '<p class="text-success"> Success! </p>',
+  styles: [
+    `
+      p {
+        padding: 20px;
+        border: 1px solid green
+      }
+    `
+  ]
 })
 
 export class SuccessComponent {
--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  template: '<p class="text-danger"> Warning! <p>'
+  template: '<p class="text-danger"> Warning! </p>',
+  styles: [
+    `
+      p {
+        padding: 20px;
+        border: 1px solid red
+      }
+    `
+  ]
 })
 
 export class WarningComponent {
