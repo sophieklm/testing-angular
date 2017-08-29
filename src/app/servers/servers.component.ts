e0df2c7fb06bef043f12e704fd63a1467a57b3f6
sophieklm <sklm@live.co.uk>
2017-08-29 14:50:19 +0100

    uses ngModel

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -22,8 +22,4 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationStatus = "Server was created!";
   }
-
-  onUpdateServerName(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
 }
