3e0f6b886b0df7ce8a8c7b99f3da1ae819b1631c
sophieklm <sklm@live.co.uk>
2017-08-29 15:02:47 +0100

    adds username update

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -20,6 +20,6 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created!";
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
   }
 }
