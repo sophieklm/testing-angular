bad9dbac92e81c0396b3a762f2300b7ea3c25147
sophieklm <sklm@live.co.uk>
2017-08-29 15:10:13 +0100

    adds ngif server created

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = "No server was created";
   serverName = 'Testserver';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -20,6 +20,6 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+    this.serverCreated = true;
   }
 }
